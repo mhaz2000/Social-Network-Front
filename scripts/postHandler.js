03ec8c6682caefb853bdcfb1cf1a04df054779d2
mhaz2000 <m_78habib@yahoo.com>
2022-07-06 12:23:31 +0430

    80% is completed

---
+++ scripts/postHandler.js
@@ -0,0 +1,84 @@
+function postDetail(event) {
+    id = event.target.parentElement.id || event.target.parentElement.parentElement.id;
+    location.replace('/post-detail.html?id=' + id);
+}
+
+
+function getPost() {
+    id = location.href.split('?id=')[1];
+    fetch('http://localhost:4030/api/post/' + id, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": document.cookie.split('=')[1]
+        }
+    }).then(response => {
+        if (response.ok) {
+            response.json().then(data => {
+                getUserInfo(data.content.postOwnerId, data.content.time, data.content.currentUserAvatar, data.content.postOwnerAvatar);
+                loadPostImage(data.content.image).then(img => {
+                    document.getElementById('post-image').src = URL.createObjectURL(img);
+                    getComments();
+                });
+
+                document.getElementById('content').innerHTML = data.content.content
+            })
+        } else {
+            response.json().then(data => {})
+        }
+    });
+
+    // getComments();
+}
+
+function getUserInfo(id, createdTime, userAvatar, postOwnerAvatar) {
+    fetch('http://localhost:4030/api/user/GetUserDetail?userId=' + id, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": document.cookie.split('=')[1]
+        }
+    }).then(response => {
+        if (response.ok) {
+            response.json().then(data => {
+                userImage = document.getElementById('add-comment-user-image');
+                postUserImage = document.getElementById('post-owner-image');
+
+                userCommentInfo = document.getElementById('user-comment-info');
+                postInfo = document.getElementById('post-info');
+
+                userName = document.createElement('h4');
+                commentUserName = document.createElement('h4');
+
+                userName.innerHTML = (data.content.firstName || data.content.lastName) ?
+                    data.content.firstName + ' ' + data.content.lastName : data.content.username;
+                commentUserName.innerHTML = (data.content.firstName || data.content.lastName) ?
+                    data.content.firstName + ' ' + data.content.lastName : data.content.username;
+
+                time = document.createElement('p');
+                time.innerHTML = createdTime;
+
+                if (userAvatar)
+                    loadPostImage(userAvatar).then(img => {
+                        userImage.src = URL.createObjectURL(img);
+                    });
+                else
+                    userImage.src = './images/unknow-user-image.png';
+
+                if (postOwnerAvatar)
+                    loadPostImage(postOwnerAvatar).then(img => {
+                        postUserImage.src = URL.createObjectURL(img);
+                    });
+                else
+                    postUserImage.src = './images/unknow-user-image.png';
+
+
+                postInfo.appendChild(userName);
+                postInfo.appendChild(time);
+                userCommentInfo.appendChild(commentUserName)
+            });
+        }
+    });
+}
---
+++ scripts/userHandler.js
@@ -0,0 +1,16 @@
+function getUser(id) {
+    return fetch('http://localhost:4030/api/user/GetUserDetail?id=' + id, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": document.cookie.split('=')[1]
+        }
+    }).then(response => {
+        if (response.ok) {
+            return response.json()
+        } else {
+            response.json().then(data => {})
+        }
+    });
+}
